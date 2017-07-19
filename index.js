b56b03aac5bbe52ed0af085d716a76527cb83b59
nickandersonr <nick.anderson.r@gmail.com>
2017-07-19 13:55:38 -0600

    removed solution

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
 //Bonus: take in state and city as arguments instead of hardcoding
-var state = "CO";
-var city = "Denver";
+
 
 //Require apiKey
-
-var apiKey = require('./env.js');
 
 //Require the request library
 //What is the `request` library you ask?
 //Google `npm request` and you can find out!
 
-var request = require('request');
 
 // Set the API URL we are hitting, with our API Key and zipcode
 
-var apiUrl = "http://api.wunderground.com/api/" + apiKey + "/conditions/q/" + state + "/" + city + ".json";
-console.log(apiUrl)
 //The actual request sending
 
 request(apiUrl, function (error, response, body) {
   //Inside that callback
 
   //Print out the weather result
-  console.log('The weather in Denver is ', JSON.parse(body).current_observation.weather);
 });
